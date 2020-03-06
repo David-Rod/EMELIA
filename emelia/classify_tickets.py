2fd3c93493168ce15bf4b24a13223f58309c546c
Reed Hayashikawa <55675425+ReedHayashikawa@users.noreply.github.com>
2020-03-06 16:19:55 -0700

    Adjusted NN input, Added git ignore, reformat files

--- emelia/classify_tickets.py
+++ emelia/classify_tickets.py
@@ -1,69 +1,33 @@
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
-
-model_path = 'models/event_cause_weights.hdf5'
-
-# from keras.models import model_from_json
-
 from learning_model import get_compiled_model
 
 
 # training model 
 def classify_data(alarm_input_data, classification_label_data, filepath):
-    '''
-    def get_compiled_model():
-
-        model = tf.keras.Sequential([
-            tf.keras.layers.Dense(10, activation='softmax'),
-            tf.keras.layers.Dense(10, activation='softmax'),
-            tf.keras.layers.Dense(10, activation='softmax'),
-            tf.keras.layers.Dense(10, activation='softmax'),
-            tf.keras.layers.Dense(1, activation='sigmoid')
-        ])
-
-        model.compile(optimizer='adam',
-                      loss='binary_crossentropy',
-                      metrics=['accuracy'])
-        return model
-    '''
-     
     
-    # x_train = encode_ticket_hex_codes()[:1266]
-    # y_train = get_event_cause_val()[:1266]
-    # x_train = alarm_input_data[:1266]
-    # y_train = classification_label_data[:1266]
     x_train = alarm_input_data[:1012]
     y_train = classification_label_data[:1012]
-    
 
-    # x_test = encode_ticket_hex_codes()[1266:]
-    # y_test = get_event_cause_val()[1266:]
-    # x_test = alarm_input_data[1266:]
-    # y_test = classification_label_data[1266:]
     x_test = alarm_input_data[1012:1266]
     y_test = classification_label_data[1012:1266]
     
-
     model = get_compiled_model()
 
     # Epochs pass the data n times through the system
     history = model.fit(x_train,
                         y_train,
-                        epochs=50,
-                        batch_size=75,
+                        epochs=15,
+                        batch_size=5,
                         validation_data=(x_test, y_test),
-                        verbose=1)
+                        verbose=2)
     
     # saves the model in the filepath listed
     model.save('./models/' + filepath)
     
-    
-    # model_json_string = model.to_json()
-    # model = model_from_json(model_json_string)
-    # model.save_weights(filepath=filepath)
-    results = model.evaluate(x_test, y_test)
     # Eval accuracy of model on test data using the test labels in the file
-    # results = model.evaluate(test_data, test_labels)
+    results = model.evaluate(x_test, y_test)
+
     print(history)
     print("Results: " + str(results))
--- emelia/output.py
+++ emelia/output.py
@@ -1,55 +1,48 @@
 import time
 import csv
 import numpy as np
-
+from learning_model import prediction
 from classify_tickets import classify_data
 from data_processing import (encode_ticket_hex_codes,
                              get_event_cause_val,
                              convert_array_to_np_array)
-from predict import prediction
-
 
 def main():
+    # starting timer 
     start_time = time.time()
-    # TODO: Call the trained models with data here
+
+    # converting lists to numpy array
     encoded_hex_codes = convert_array_to_np_array(encode_ticket_hex_codes())
     event_cause_options = convert_array_to_np_array(get_event_cause_val())
    
-    # event_cause_classification = classify_data(encoded_hex_codes,
-    #                                           event_cause_options,
-    #                                          'event_cause_weights.hdf5')
-    
+    # training & saving the model
+    classify_data(encoded_hex_codes, event_cause_options, 'event_cause_weights.hdf5')
     
     # 20 percent that needs to be tested
     predict_input_hex = encoded_hex_codes[1266:]
     predict_actual = event_cause_options[1266:]
     
+    # calling prediction from predict.py and returns array of confidence values
+    result_array = prediction(predict_input_hex)
     
-    result_array = prediction( predict_input_hex )
-    
-    
-    # print statement to check array lengths: all 317
-    # print("predict_input_hex length: " + str(len(predict_input_hex)) )
-    # print("predict_actual length: " + str(len(predict_actual)) )
-    # print("predict_result length: " + str(len(result_array)) )
-    
+    '''result.csv contains corresp. hex code, 
+    the correct classification (Actual), 
+    predicition (result) and if the 
+    prediction was correct (Correctly Classified)
+    '''
     with open('result.csv', mode='w') as csv_file:
            fieldnames = ['Hex Code', 'Actual', 'Result', 'Correctly Classified']
            writer = csv.DictWriter(csv_file, fieldnames = fieldnames)
 
            writer.writeheader()
            
-           #list(predict_actual)
-           #list(result_array)
+           # Converting np arrays to lists
            predict_actual = np.array(predict_actual).tolist()
            result_array = np.array(result_array).tolist()
-           #np.asarray(predict_actual)
-           #np.asarray(result_array)
            
            length_of_array = len(predict_input_hex) #317
            
            num_of_vals = len(predict_actual[0]) #9
-           
            
            # loops through rows
            for rows in range(length_of_array):
@@ -57,35 +50,27 @@ def main():
                   boolean = False
                   position = 0
                   
+                  # gets the index of largest confidence value
                   for index in range(num_of_vals):
                          if result_array[rows][index] == greatest_percent:
                                 position = index
                                 
+                  # if the index of largest confidence value equals the index of actual classification 
                   for working_index in range(num_of_vals):
                          if predict_actual[rows][working_index] == 1 and working_index == position:
                                 boolean = True
-                                
-                  
-                  #position_of_actual = predict_actual.index(max(predict_actual))
-                  #position_of_actual = predict_actual[np.array([(max(predict_actual))])]
-                  #position_of_actual = np.where(predict_actual == max(predict_actual))
-                  
-                  #position_of_result = result_array.index(max(result_array))
-                  #position_of_result = result_array[np.array([(max(result_array))])]
-                  #position_of_result = np.where(result_array == max(result_array))
        
+                  
                   writer.writerow({'Hex Code': predict_input_hex[rows], 
                                    'Actual': predict_actual[rows], 
                                    'Result': result_array[rows],
                                    'Correctly Classified': boolean})
            csv_file.close()
-           
-                              
 
-    # write_to_file(event_cause_classification, 'output.txt')
-    # print(event_cause_classification)
-
+    # end timer 
     end_time = time.time()
+    
+    # run time 
     runtime = round(end_time - start_time, 2)
     avg_time_per_ticket = round(runtime / 1583, 5)
 
