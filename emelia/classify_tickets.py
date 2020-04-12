d9564da0cf4459faee089aa13095dac022b9167b
David <davidrod805@gmail.com>
2020-04-11 21:11:43 -0700

    ENH: Making changes to the learning model and input data

--- emelia/classify_tickets.py
+++ emelia/classify_tickets.py
@@ -1,33 +1,48 @@
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
+from sklearn.model_selection import train_test_split
 
 from learning_model import get_compiled_model
 
 
-# training model 
+# training model
 def classify_data(alarm_input_data, classification_label_data, filepath):
-    
-    x_train = alarm_input_data[:1012]
-    y_train = classification_label_data[:1012]
 
-    x_test = alarm_input_data[1012:1266]
-    y_test = classification_label_data[1012:1266]
-    
+    x_ticket_data = classification_label_data[:1266]
+    y_alarm_data = alarm_input_data[:1266]
+
+    x_train, x_test, y_train, y_test = train_test_split(x_ticket_data,
+                                                        y_alarm_data,
+                                                        test_size=0.20,
+                                                        random_state=0)
+
+    # x_train = alarm_input_data[:1012]
+    # x_train = alarm_input_data[316:1050]   USED LAST
+    # y_train = classification_label_data[:1012]
+    #y_train = classification_label_data[316:1050]   USED LAST
+
+    # x_test = alarm_input_data[1012:1266]
+    #x_test = alarm_input_data[1050:]   USED LAST
+    # y_test = classification_label_data[1012:1266]
+    #y_test = classification_label_data[1050:]   USED LAST
+
     model = get_compiled_model()
 
     # Epochs pass the data n times through the system
     history = model.fit(x_train,
                         y_train,
-                        epochs=15,
-                        batch_size=5,
+                        epochs=10,
+                        batch_size=50,
                         validation_data=(x_test, y_test),
                         verbose=2)
-    
+
     # saves the model in the filepath listed
     model.save('./models/' + filepath)
-    
-    # Eval accuracy of model on test data using the test labels in the file
-    results = model.evaluate(x_test, y_test)
 
-    print(history)
-    print("Results: " + str(results))
+    # Eval accuracy of model on test data using the test labels in the file
+    test_results = model.evaluate(x_test, y_test)
+    train_results = model.evaluate(x_train, y_train)
+
+    print(history.model)
+    print("Test Evaluation: " + str(test_results))
+    print("Train Evaluation: " + str(train_results))
--- emelia/learning_model.py
+++ emelia/learning_model.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from tensorflow import keras
-from keras.models import load_model
+# from keras.metrics import categorical_accuracy
+
+# from keras.models import load_model
 
 # import EMELIA.data_processing as dp
 # from EMELIA.data_processing import encode_ticket_hex_codes, get_event_cause_val
@@ -13,12 +15,19 @@ def get_compiled_model():
     ###################### INPUT  ########################
 
     # Embedding layer, looks up embedding vector for each word
-    model.add(keras.layers.Dense(32, input_shape=(74,)))
-
+    model.add(keras.layers.Dense(125,
+                                 input_shape=(101,),
+                                 kernel_initializer='normal'))
+    '''
+    model.add(keras.layers.Dropout(32,
+                                 activation=tf.nn.softmax,
+                                 kernel_constraint=unit_norm(1)))
+    '''
     ###################### HIDDEN ########################
 
     # 16 hidden units in the hidden layer using activation function
-    model.add(keras.layers.Dense(16, activation=tf.nn.softmax))
+    model.add(keras.layers.Dense(50, activation=tf.nn.relu))
+    model.add(keras.layers.Dropout(.5))
 
     ###################### OUTPUT ########################
     model.add(keras.layers.Dense(9, activation=tf.nn.softmax))
@@ -31,18 +40,15 @@ def get_compiled_model():
     # or 0, measure distance of probability distributions.
         # binary_crossentropy selected based on the 0 or 1 values
         # accuracy is provided to the metric param to train the model
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['acc'])
 
     return model
 
 
 # pass in file that contains only 20 percent of hex values that need to be predicted
 def prediction(test_input):
-
-       modelpath = './models/event_cause_weights.hdf5'
-
-       model = tf.keras.models.load_model(modelpath)
-       
-       return model.predict(test_input)
+    modelpath = "./models/event_cause_weights.hdf5"
+    model = tf.keras.models.load_model(modelpath)
+    return model.predict(test_input)
